1135749dc20a0a4f5d6b1fe00d4839e205d17b45
YuyaKobayashi <yuya.kobayashi1023@gmail.com>
2025-05-31 19:00:33 +0900

    auto push

---
+++ nvim-setting/push-and-update.ps1
@@ -0,0 +1,14 @@
+# push-and-sync.ps1
+
+# 1. push する
+git push
+
+# 2. push が成功したら、init.lua を AppData にコピーする
+if ($LASTEXITCODE -eq 0) {
+    $sourcePath = "$env:USERPROFILE\src\any-settings\nvim-settings\init.lua"
+    $destPath = "$env:LOCALAPPDATA\nvim\init.lua"
+    Copy-Item $sourcePath $destPath -Force
+    Write-Host "init.lua copied to $destPath"
+} else {
+    Write-Host "Git push failed. init.lua not copied."
+}
